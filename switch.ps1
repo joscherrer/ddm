748e9ceb0338ae29fc0ab160d8a4556bf82165ad
joscherrer <jonathan.s.scherrer@gmail.com>
2021-03-17 17:59:20 +0100

    wip

--- switch.ps1
+++ switch.ps1
@@ -1,16 +1,29 @@
 param([string] $State)
 
-# 660X879Q0C5L : u3417w
-# 7MT0167B2YNL : u2415 droite
+# 660X879Q0C5L: u3417w
+#     Home: mDP1
+#     Work: DP1
+# 7MT0167B2YNL: u2415 droite
+#     Home: mDP1
+#     Work: DP1
+# 7MT0167B2Y5L: u2415 gauche
+#     Home: HDMI1
+#     Work: HDMI2
 
-if ($State -eq "work") {
-    # 7MT0167B2YNL : u2415 droite
-    & "C:\Program Files (x86)\Dell\Dell Display Manager\ddm.exe" /7MT0167B2YNL:SetActiveInput "mDP1"
-    Start-Sleep -Milliseconds 8000
-    # 660X879Q0C5L : u3417w
-    & "C:\Program Files (x86)\Dell\Dell Display Manager\ddm.exe" /660X879Q0C5L:SetActiveInput "mDP1"
-} elseif ($State -eq "home") {
-    & "C:\Program Files (x86)\Dell\Dell Display Manager\ddm.exe" /660X879Q0C5L:SetActiveInput "DP1"
-    Start-Sleep -Milliseconds 8000
-    & "C:\Program Files (x86)\Dell\Dell Display Manager\ddm.exe" /7MT0167B2YNL:SetActiveInput "DP1"
+# $bn = 20; & "C:\Program Files (x86)\Dell\Dell Display Manager\ddm.exe" /7MT0167B2Y5L:SetBrightnessLevel "$bn" /660X879Q0C5L:SetBrightnessLevel "$bn" /7MT0167B2YNL:SetBrightnessLevel "$bn"
+
+if ($State -eq "work")
+{
+    & "C:\Program Files (x86)\Dell\Dell Display Manager\ddm.exe" /7MT0167B2Y5L:SetActiveInput "HDMI2" /660X879Q0C5L:SetActiveInput "mDP1" /7MT0167B2YNL:SetActiveInput "mDP1" # u2415 gauche
+    # Start-Sleep -Milliseconds 8000
+    # & "C:\Program Files (x86)\Dell\Dell Display Manager\ddm.exe" /660X879Q0C5L:SetActiveInput "mDP1" # u3417w
+    # Start-Sleep -Milliseconds 8000
+    # & "C:\Program Files (x86)\Dell\Dell Display Manager\ddm.exe" /7MT0167B2YNL:SetActiveInput "mDP1" # u2415 droite
+} elseif ($State -eq "home")
+{
+    & "C:\Program Files (x86)\Dell\Dell Display Manager\ddm.exe" /7MT0167B2Y5L:SetActiveInput "HDMI1" /7MT0167B2YNL:SetActiveInput "DP1" /660X879Q0CNL:SetActiveInput "DP1" # u2415 gauche
+    # Start-Sleep -Milliseconds 8000
+    # & "C:\Program Files (x86)\Dell\Dell Display Manager\ddm.exe" /7MT0167B2YNL:SetActiveInput "DP1" # u2415 droite
+    # Start-Sleep -Milliseconds 8000
+    # & "C:\Program Files (x86)\Dell\Dell Display Manager\ddm.exe" /660X879Q0CNL:SetActiveInput "DP1" # u3417w
 }
